adb6aaf0e3599884a83c6ae93db62d454ce4c396
unknown <chromegold70@gmail.com>
2021-10-07 22:30:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Container, Header, Body } from './styles/App.styles'
+
+const src: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <h1>Sistema Financeiro</h1>
+      </Header>
+
+      <Body></Body>
+    </Container>
+  )
+}
+
+export default src
---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { title } from 'process'
+
+export const items = [
+  {
+    date: new Date(2021, 9, 15),
+    category: 'food',
+    title: 'McDonalds',
+    value: 32.12
+  },
+  {
+    date: new Date(2021, 9, 15),
+    category: 'food',
+    title: 'Burger King',
+    value: 28
+  },
+  {
+    date: new Date(2021, 9, 16),
+    category: 'rent',
+    title: 'Aluguel Apt',
+    value: 2300
+  },
+  {
+    date: new Date(2021, 9, 18),
+    category: 'salary',
+    title: 'Salário',
+    value: 4500
+  }
+]
